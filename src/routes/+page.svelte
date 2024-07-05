a5a83a2a122f7e9fa7786f1310e6d9aa829f8de7
Muhamad Bestagi Romadhon <bestagi.egi@live.com>
2024-07-05 17:55:13 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Deatsi: DE Advance Technology Service Indonesia</title>
+	<meta name="Deatsi" content="DE Advance Technology Service Indonesia" />
+</svelte:head>
+
+<div class="content">
+	<h1 class="title">
+		Deatsi
+		<div class="aurora">
+			<div class="aurora__item"></div>
+			<div class="aurora__item"></div>
+			<div class="aurora__item"></div>
+			<div class="aurora__item"></div>
+		</div>
+	</h1>
+	<h3 class="subtitle">DE Advance Technology Service Indonesia</h3>
+</div>
